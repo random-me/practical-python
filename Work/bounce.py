186907667547a461a98262769f158ab8276df7f4
random-me <random-me.disabled079@simplelogin.com>
2025-03-06 16:03:44 +1100

    1.5 bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100 # Meters
+bounce = 0 # Bounces done
+
+while bounce < 10:
+    bounce = bounce + 1
+    height = height * (3/5)
+    print(bounce, round(height, 4))
