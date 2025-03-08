59c974d9bf71693863439acc1685b266c9f675d6
random-me <random-me.disabled079@simplelogin.com>
2025-03-08 14:39:36 +1100

    1.27 : reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+infile = 'Data/portfolio.csv'
+total_value = 0
+
+with open(infile, 'rt') as file:
+    headers = next(file)
+    for line in file:
+        fields = line.split(',')
+        row_total = int(fields[1]) * float(fields[2])
+        total_value = total_value + row_total
+
+print("Total cost ", total_value)
