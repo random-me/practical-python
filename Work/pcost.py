2f3cc6541e8fab53f83c5285ea4d0fac4413d9b8
random-me <random-me.disabled079@simplelogin.com>
2025-03-08 19:56:37 +1100

    1.32 using a library function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 
 def portfolio_cost(filename):
     total_value = 0
-    with open(filename, 'rt') as file:
-        headers = next(file)
-        for line in file:
-            fields = line.split(',')
-            try:
-                row_total = int(fields[1]) * float(fields[2])
-            except ValueError:
-                print("Couldn't parse :", line.strip())
-                next
-            total_value = total_value + row_total
+    file = open(filename)
+    rows = csv.reader(file)
+    headers = next(rows)
+
+    for row in rows:
+        fields = row.split(',')
+        try:
+            row_total = int(fields[1]) * float(fields[2])
+        except ValueError:
+            print("Couldn't parse :", row.strip())
+            next
+        total_value = total_value + row_total
     return float(total_value)
 
 infile = 'Data/portfolio.csv'
