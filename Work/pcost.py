b362359e65375a257d544c9324b4fc5d4417641d
random-me <random-me.disabled079@simplelogin.com>
2025-03-08 20:03:20 +1100

    1.33 reading from the command line

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.27
 import csv
+import sys
 
 def portfolio_cost(filename):
     total_value = 0
@@ -9,16 +10,19 @@ def portfolio_cost(filename):
     rows = csv.reader(file)
     headers = next(rows)
 
-    for row in rows:
-        fields = row.split(',')
+    for fields in rows:
         try:
             row_total = int(fields[1]) * float(fields[2])
         except ValueError:
-            print("Couldn't parse :", row.strip())
+            print("Couldn't parse :", fields)
             next
         total_value = total_value + row_total
     return float(total_value)
 
-infile = 'Data/portfolio.csv'
-cost = portfolio_cost(infile)
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
