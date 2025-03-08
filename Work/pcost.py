95a3a9956ed8512b4abc2bee9009472309b432c9
random-me <random-me.disabled079@simplelogin.com>
2025-03-08 19:51:24 +1100

    1.31 : error handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
+
+def portfolio_cost(filename):
+    total_value = 0
+    with open(filename, 'rt') as file:
+        headers = next(file)
+        for line in file:
+            fields = line.split(',')
+            try:
+                row_total = int(fields[1]) * float(fields[2])
+            except ValueError:
+                print("Couldn't parse :", line.strip())
+                next
+            total_value = total_value + row_total
+    return float(total_value)
+
 infile = 'Data/portfolio.csv'
-total_value = 0
-
-with open(infile, 'rt') as file:
-    headers = next(file)
-    for line in file:
-        fields = line.split(',')
-        row_total = int(fields[1]) * float(fields[2])
-        total_value = total_value + row_total
-
-print("Total cost ", total_value)
+cost = portfolio_cost(infile)
+print('Total cost:', cost)
