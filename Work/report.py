ec4a5d4602d8bdd542831845a63225ebce200f40
random-me <random-me.disabled079@simplelogin.com>
2025-03-10 15:42:05 +1100

    2.4 a list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,28 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+def read_portfolio(filename):
+    file = open(filename)
+    contents = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                contents.append((row[0], int(row[1]), float(row[2])))
+            except ValueError:
+                print("Couldn't parse: ", row)
+                next
+
+    return contents
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+list = read_portfolio(filename)
+print(list)
